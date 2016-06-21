039bac0588249a443972cb35b0bc6586c1738ff1
rhhxj <20141105047M@admindeiMac-08.local>
2016-06-21 09:05:16 +0800

    20个数组从小到大进行排序
    闭包（即一些小的匿名代码块），可以像函数一样使用。可以很方便的将闭包传给其他函数，告诉它们应当如何执行某一个任务。
    使用sort方法和闭包进行数组排序
    sort方法返回一个数组的有序版本。（sort方法即为Swift2.0之前的sorted方法。而原来的sort方法改名为sortInPlace，
    用来将原数组修改成有序版本）

--- 20shuzupaixu/main.swift
+++ 20shuzupaixu/main.swift
@@ -8,5 +8,11 @@
 
 import Foundation
 
-print("Hello, World!")
+let numbers = [12,25,1,35,27,24,54,23,76,87,59,43,26,43,15,16,8,98,11,74]
 
+let numbersSorted = numbers.sort({ (n1: Int, n2: Int) -> Bool in
+
+return n2 > n1
+})
+
+print(numbersSorted)
