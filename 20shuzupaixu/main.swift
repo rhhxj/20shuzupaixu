0e60f41442b9206f98fb24f276f7c7da46a75d35
rhhxj <20141105047M@admindeimac-08.local>
2016-06-21 08:44:24 +0800

    Initial Commit

---
+++ 20shuzupaixu/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  20shuzupaixu
+//
+//  Created by 20141105047 on 16/6/21.
+//  Copyright © 2016年 20141105047. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
